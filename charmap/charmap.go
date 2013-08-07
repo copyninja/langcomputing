2c1c850f56f8b2f38b50bff168fb1ccc6a793a6d
Vasudev Kamath <kamathvasudev@gmail.com>
2013-08-07 20:08:44 +0530

    Code improvements

--- charmap/charmap.go
+++ charmap/charmap.go
@@ -1,5 +1,19 @@
 package charmap
 
+import "strings"
+
+// Private types for rune and string slices
+type runeSequence []rune
+type stringSequence []string
+
+type sequenceIndex interface {
+	index(char rune) int
+}
+
+// Private map to hold all sequences
+type charMap map[string]sequenceIndex
+
+// Languagewise unicode ranges
 var langBases = map[string]int{
 	"en_US": 0,
 	"en_IN": 0,
@@ -14,68 +28,104 @@ var langBases = map[string]int{
 	"ml_IN": '\u0D01',
 }
 
-var devaAlphabets = make([]rune, 80)
-var bengAlphabets = make([]rune, 80)
-var guruAlphabets = make([]rune, 80)
-var gujrAlphabets = make([]rune, 80)
-var oryaAlphabets = make([]rune, 80)
-var tamlAlphabets = make([]rune, 80)
-var teluAlphabets = make([]rune, 80)
-var kndaAlphabets = make([]rune, 80)
-var mlymAlphabets = make([]rune, 80)
+// Slices to hold unicode range for each languagges
+var devaAlphabets = make(runeSequence, 80)
+var bengAlphabets = make(runeSequence, 80)
+var guruAlphabets = make(runeSequence, 80)
+var gujrAlphabets = make(runeSequence, 80)
+var oryaAlphabets = make(runeSequence, 80)
+var tamlAlphabets = make(runeSequence, 80)
+var teluAlphabets = make(runeSequence, 80)
+var kndaAlphabets = make(runeSequence, 80)
+var mlymAlphabets = make(runeSequence, 80)
 
-var charmap = map[string][]rune{
-	"hi_IN": devaAlphabets,
-	"bn_IN": bengAlphabets,
-	"pa_IN": guruAlphabets,
-	"gu_IN": gujrAlphabets,
-	"or_IN": oryaAlphabets,
-	"ta_IN": tamlAlphabets,
-	"te_IN": teluAlphabets,
-	"kn_IN": kndaAlphabets,
-	"ml_IN": mlymAlphabets,
+// Soundex values for English alphabet series
+var soundexEnglish = runeSequence{'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'}
+
+// Soundex values for Indian language unicode series.
+var soundexIndic = runeSequence{'0', 'N', '0', '0', 'A', 'A', 'B', 'B', 'C', 'C', 'P', 'Q', '0', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'F', 'F', 'F', 'F', 'G', 'H', 'H', 'H', 'H', 'G', 'I', 'I', 'I', 'I', 'J', 'K', 'K', 'K', 'K', 'L', 'L', 'M', 'M', 'M', 'M', 'N', 'O', 'P', 'P', 'Q', 'Q', 'Q', 'R', 'S', 'S', 'S', 'T', '0', '0', '0', '0', 'A', 'B', 'B', 'C', 'C', 'P', 'P', 'E', 'D', 'D', 'D', 'D', 'E', 'E', 'E', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'E', '0', '0', '0', '0', '0', '0', '0', '0', 'P', 'Q', 'Q', 'Q', '0', '0', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'J', 'J', 'Q', 'P', 'P', 'F'}
+
+// ISO15919 series specific to Indian languages
+var iso15919IndicSeries = stringSequence{`m̐`, `ṁ`, `ḥ`, ``, `a`, `ā`, `i`, `ī`, `u`, `ū`, `ṛ`, `ḷ`, `ê`, `e`, `ē`, `ai`, `ô`, `o`, `ō`, `au`, `ka`, `kha`, `ga`, `gha`, `ṅa`, `ca`, `cha`, `ja`, `jha`, `ña`, `ṭa`, `ṭha`, `ḍa`, `ḍha`, `ṇa`, `ta`, `tha`, `da`, `dha`, `na`, `ṉa`, `pa`, `pha`, `ba`, `bha`, `ma`, `ya`, `ra`, `ṟa`, `la`, `ḷa`, `ḻa`, `va`, `śa`, `ṣa`, `sa`, `ha`, ``, ``, ``, `'`, `ā`, `i`, `ī`, `u`, `ū`, `ṛ`, `ṝ`, `ê`, `e`, `ē`, `ai`, `ô`, `o`, `ō`, `au`, ``, ``, ``, `oṃ`, ``, ``, ``, ``, ``, ``, ``, `qa`, `ḵẖa`, `ġ`, `za`, `ṛa`, `ṛha`, `fa`, `ẏa`, `ṝ`, `ḹ`, `ḷ`, `ḹ`, `.`, `..`, `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `…`, ``, ``, ``, ``, ``, ``, ``}
+
+// IPA series specific for Indian languages
+var ipaIndicSeries = stringSequence{`m`, `m`, ``, ``, `ə`, `aː`, `i`, `iː`, `u`, `uː`, `r̩`, `l̩`, `æ`, `e`, `eː`, `ɛː`, `ɔ`, `o`, `oː`, `ow`, `kə`, `kʰə`, `gə`, `gʱə`, `ŋə`, `ʧə`, `ʧʰə`, `ʤə`, `ʤʱə`, `ɲə`, `ʈə`, `ʈʰə`, `ɖə`, `ɖʱə`, `ɳə`, `t̪ə`, `t̪ʰə`, `d̪ə`, `d̪ʱə`, `n̪ə`, `nə`, `pə`, `pʰə`, `bə`, `bʱə`, `mə`, `jə`, `ɾə`, `rə`, `lə`, `ɭə`, `ɻə`, `ʋə`, `ɕə`, `ʂə`, `sə`, `ɦə`, ``, ``, ``, `ഽ`, `aː`, `i`, `iː`, `u`, `uː`, `r̩`, `l̩`, `e`, `eː`, `ɛː`, `ɔ`, `o`, `oː`, `ow`, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, `ow`, ``, ``, ``, ``, ``, ``, ``, ``, `r̩ː`, `l̩ː`, ``, ``, ``, ``, `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `൰`, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``, ``}
+
+// Map to hold rune sequence of each languages
+var langMap = charMap{
+	"hi_IN":      devaAlphabets,
+	"bn_IN":      bengAlphabets,
+	"pa_IN":      guruAlphabets,
+	"gu_IN":      gujrAlphabets,
+	"or_IN":      oryaAlphabets,
+	"ta_IN":      tamlAlphabets,
+	"te_IN":      teluAlphabets,
+	"kn_IN":      kndaAlphabets,
+	"ml_IN":      mlymAlphabets,
+	"soundex_en": soundexEnglish,
+	"soundex_in": soundexIndic,
+	"IPA":        ipaIndicSeries,
+	"ISO15919":   iso15919IndicSeries,
 }
 
-func initializeUnicodeRange(slice []rune, begin, length int) {
+// Map to hold ISO and IPA string sequences
+// var iso15919IPACharmap = stringMap{
+// 	"ISO15919": iso15919IndicSeries,
+// 	"IPA":      ipaIndicSeries,
+// }
+
+func initializeUnicodeRange(slice runeSequence, begin, length int) {
 	for i := 0; i < length; i++ {
 		slice[i] = rune(begin + i)
 	}
 }
 
 func init() {
-	for key, value := range charmap {
-		initializeUnicodeRange(value, langBases[key], 80)
+	for key, value := range langMap {
+		if _, ok := value.(runeSequence); ok {
+			initializeUnicodeRange(value.(runeSequence), langBases[key], len(value.(runeSequence)))
+		}
 	}
 }
 
-func LanguageOf(char rune) string {
-	for lang, langRange := range charmap {
-		for _, value := range langRange {
-			if value == char {
-				return lang
-			}
+func (r runeSequence) index(char rune) int {
+	for i, value := range r {
+		if value == char {
+			return i
 		}
 	}
 
+	return -1
+}
+
+func (s stringSequence) index(char rune) int {
+	for i, value := range s {
+		if strings.ContainsRune(value, char) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func LanguageOf(char rune) string {
+	for lang, langRange := range langMap {
+		if langRange.index(char) != -1 {
+			return lang
+		}
+	}
+	// Still not found then something wrong
 	return "unknown"
 }
 
 func CharCompare(char1, char2 rune) bool {
-	char1Index, char2Index := -1, -1
 
 	if char1 == char2 {
 		return true
 	}
 
-	for _, slice := range charmap {
-		for index, value := range slice {
-			if value == char1 {
-				char1Index = index
-			} else if value == char2 {
-				char2Index = index
-			}
-		}
-	}
+	char1Index := langMap[LanguageOf(char1)].index(char1)
+	char2Index := langMap[LanguageOf(char2)].index(char2)
 
 	if char1Index == char2Index {
 		return true
--- charmap/charmap_test.go
+++ charmap/charmap_test.go
@@ -3,15 +3,34 @@ package charmap
 import "testing"
 
 func TestLanguageOf(t *testing.T) {
-	const in, out = 'ಅ', "kn_IN"
-	if x := LanguageOf(in); x != out {
-		t.Errorf("LanguageOf(%v) = %s we need %s", in, x, out)
+
+	inArray := []rune{'ಅ', 'ḥ', 'ɭ', '\u0d0a'}
+	outArray := []string{"kn_IN", "ISO15919", "IPA", "ml_IN"}
+
+	for index, value := range inArray {
+		if x, output := LanguageOf(value), outArray[index]; x != output {
+			t.Errorf("LanguageOf(%v) = %s we need %s", value, x, output)
+		}
 	}
+
 }
 
 func TestCharCompare(t *testing.T) {
-	const in1, in2 = 'ಅ', 'अ'
-	if x := CharCompare(in1, in2); !x {
-		t.Errorf("CharCompare(%v, %v) = %v we need %v", in1, in2, x, true)
+	inArray := []rune{'ँ', 'అ'}
+	outArray := []rune{'ಁ', 'അ'}
+
+	// multiByteIn := []string {`aː`, `ಆ` }
+	// multiByteOut := []string {`ആ`, `ā`}
+
+	for index, value := range inArray {
+		if x := CharCompare(value, outArray[index]); !x {
+			t.Errorf("CharCompare(%v, %v) = %v we need %v", value, outArray[index], x, true)
+		}
 	}
+
+	// for index, value := range multiByteIn {
+	// 	if x := CharCompare(value, multiByteOut[index]); !x {
+	// 		t.Errorf("CharCompare(%v, %v) = %v we need %v", value, multiByteOut[index], x, true)
+	// 	}
+	// }
 }
