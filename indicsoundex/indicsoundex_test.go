aa3196d8bd6bbcd8686bfd6fa9318c79b5bf2819
Vasudev Kamath <kamathvasudev@gmail.com>
2013-08-15 10:09:15 +0530

    English test cases are now enabled

--- indicsoundex/indicsoundex_test.go
+++ indicsoundex/indicsoundex_test.go
@@ -3,10 +3,8 @@ package indicsoundex
 import "testing"
 
 func TestCalculate(t *testing.T) {
-	// inArray := []string {"vasudeva", "kamath", "ವಾಸುದೇವ", "वासुदॆव"}
-	inArray := []string{`ವಾಸುದೇವ`, `वासुदॆव`}
-	// outArray := []string {"vA2C3D1A", "kA5A3000", "ವASCKDR0", "वASCKDR0" }
-	outArray := []string{`ವASCKDR0`, `वASCKDR0`}
+	inArray := []string{"vasudeva", "kamath", "ವಾಸುದೇವ", "वासुदॆव"}
+	outArray := []string{"v231", "k53", "ವASCKDR0", "वASCKDR0"}
 
 	for index, value := range inArray {
 		if x, output := Calculate(value, 8), outArray[index]; x != output {
